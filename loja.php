69167e559c7c439a94b8c2d5c5d970b636ac388f
Alison <alisonmozer@gmail.com>
2023-02-13 11:58:40 -0300

    upando github

---
+++ loja.php
@@ -0,0 +1,32 @@
+<?php
+    
+    class Loja
+    {
+        var $Nome = ""; //a palavra "var" é colocada para definir cada atributo de uma classe, PHP nao existe declaração do tipo das variaveis
+        var $Lista_Produtos = []; //para definir "array" em PHP so precisa atribui o simbolo [] um array é uma lista de valores 
+        var $Lista_Clientes = [];
+
+        function Inserir_Produto( $produto ) //METODOS
+        {
+            $this->Lista_Produtos[] = $produto; //"this" é um objeto especial, para acessar os atributos e métodos da propria classe
+
+        }
+    }
+    $filial = new Loja();
+    $filial->Inserir_Produto("PRODUTO");
+
+    echo    "<!doctype html>";
+    echo    "<html>";
+    echo    "<head> <meta charset='utf-8'> <title> Loja de F1 </title></head>";
+    echo    "<body>";
+
+    $quant = count($filial->Lista_Produtos);
+    for ($i = 0; $i < $quant; $i++)
+    {
+        echo    $filial->Lista_Produtos[$i] . "<br>";
+    }
+
+    echo    "</body>";
+    echo    "</html>";
+    
+?>
