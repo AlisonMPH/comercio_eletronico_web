c20391ac899765c64c7aa606a0b8aac6b1b94bcb
Alison <alisonmozer@gmail.com>
2023-04-03 21:48:46 -0300

    correcao gpt

--- alunoinfo/bd_produto.php
+++ alunoinfo/bd_produto.php
@@ -1,19 +1,18 @@
 <?php
     include 'doc_html.php';
 
-        function Consulta_Dados ($p_Conexao_BD)
+    function Consulta_Dados ($p_Conexao_BD)
     {
         $listagem = "";
         $sql = "SELECT * FROM Produto;";
-        $REGISTROS = $p_Conexao_BD-> query($sql);
+        $REGISTROS = $p_Conexao_BD->query($sql);
 
         $listagem .= "<hr>Listagem dos dados<hr>";
         while($registro = $REGISTROS->fetch_assoc())
         {
-            $listagem .= "ID: ".$registro["XP_Produto"]."- Nome: ".$registro["P_Nome"]." - Preço: ".$registro["P_Preco"]." Descrição: ".$registro["P_Descricao"]."<br>";
+            $listagem .= "ID: ".$registro["XP_Produto"]." - Nome: ".$registro["P_Nome"]." - Preço: ".$registro["P_Preco"]." - Descrição: ".$registro["P_Descricao"]."<br>";
         }
         $listagem .= "<hr>Fim dos dados<hr>";
         return $listagem;
     }
-
 ?>
